e5afa850bccc43da7fd09d64b49be5f35b4d67ea
killian marchand <l2info025@ic2s124-07.univ-lemans.fr>
2018-01-30 16:06:10 +0100

    proto laby

--- proto.c
+++ proto.c
@@ -1,14 +1,35 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-#define M 20
 #define N 20
+#define MAX 0
+#define MIN 1
 
-void gener_tab(case_t mat[N][M]){
+void affichage(case_t mat[N][N]){
 	
-	int mur_libre;
-	int i;	
-	mur_libre=rand()	
+}
+
+void init_mur(case_t mat[N][N]){ /*initialisation des bordures de la map*/
+	for(i=0;i<N;i++){
+		for(j=0;j<N;j++){
+			
+}
+
+void gener_tab(case_t mat[N][N]){ /*generation de l'interieur de la map*/
 	
+	mur_t mur;
+	int i,j;
+	for(i=0;i<N;i++){
+		for(j=0;j<N;j++){
+					
+	
+			mat[i][j].mur.murN= (srand() % (MAX - MIN + 1)) + MIN;  //random pour definir si mur ou non
+			mat[i][j].mur.murN= (srand() % (MAX - MIN + 1)) + MIN;
+			mat[i][j].mur.murN= (srand() % (MAX - MIN + 1)) + MIN;
+			mat[i][j].mur.murN= (srand() % (MAX - MIN + 1)) + MIN;
+		}
+	}
 	
 	
 }
