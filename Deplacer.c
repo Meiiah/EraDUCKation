a18739dd7a5526bfba2bf76701ca4067859ceb68
kilmhd <35963499+kilmhd@users.noreply.github.com>
2018-02-01 14:18:52 +0100

    Update Deplacer.c

--- Deplacer.c
+++ Deplacer.c
@@ -79,6 +79,80 @@ int voit_nourriture(int i,int j,int k){
     return -1;  // au cas où mais pas besoin normalement
 }
 
+int voit_nourriture(int i,int j,int k){
+    //une direction, toute la ligne jusqua un mur
+    int l = 1;
+    int mur[4] = {0,0,0,0};
+    //on renvoit la direction ou il voit le miam miam et -1 si il voit pas (choisi la nouriture la plus proche
+
+    //dir 1
+    while(true){//tant qu on a pas de mur partout(break) et qu on n'a pas de fruit (break)
+
+        /*
+        SI pas de mur enregistré on regarde si il y a un fruit, si c est le cas on break,
+        puis on regarde si il y a un mur, et si il y a on memorise qu il y en a un et
+            on verifie que tous les murs soient pas trouvés
+    */
+//////////////////////////////////////////////////////////////////////
+                //pour i croissant direction est
+
+        if(mur[0]==0){
+            if(matrice[i+l][j].pres_nourriture==1){ //si presence de nourriture sur la ligne
+                return 1;
+            }
+        }
+        if(matrice[i+l][j].mur.murE == 0){
+            if(mur[1]==1 && mur[2]==1 && mur[3]==1) // sipas de nourriture visible retourner -1
+                return -1;
+            mur[0] = 1;
+        }
+
+/////////////////////////////////////////////////////////////////////
+                //pour i décroissant direction ouest
+
+        if(mur[1]==0){
+            if(matrice[i-l][j].pres_nourriture==1){
+                return 2;
+            }
+        }
+        if(matrice[i-l][j].mur.murO == 0){
+            if(mur[0]==1 && mur[2]==1 && mur[3]==1) // sipas de nourriture visible retourner -1
+                return -1;
+            mur[1] = 1;
+        }
+
+/////////////////////////////////////////////////////////////////////
+                //pour j croissant direction sud
+
+        if(mur[2]==0){
+            if(matrice[i][j+l].pres_nourriture==1){
+                return 3;
+            }
+        }
+        if(matrice[i][j+l].mur.murS == 0){
+            if(mur[0]==1 && mur[1]==1 && mur[3]==1) // sipas de nourriture visible retourner -1
+                return -1;
+            mur[2] = 1;
+        }
+
+/////////////////////////////////////////////////////////////////////
+                //pour j décroissant direction Nord
+
+        if(mur[3]==0){
+            if(matrice[i][j-l].pres_nourriture==1){
+                return 4;
+            }
+        }
+        if(matrice[i][j-l].mur.murN == 0){
+            if(mur[0]==1 && mur[1]==1 && mur[2]==1) // sipas de nourriture visible retourner -1
+                return -1;
+            mur[3] = 1;
+        }
+        l++;
+    }
+    return -1;  // au cas où mais pas besoin normalement
+}
+
 /*=======================================================================================================================*/
 
 void deplacer(void){
