3f20bef2537692edc1e0b1e93069496b14029d8f
MaximeTouze <35963692+MaximeTouze@users.noreply.github.com>
2018-02-01 14:07:44 +0100

    Update Sauvegarde.c
    Commentaires
    
    Reste a tester et ajouter en include les globales

--- Sauvegarde.c
+++ Sauvegarde.c
@@ -6,28 +6,32 @@ int N =10;
 case_t* matrice;
 int gen = 12;
 
-void sauvegarde(){
+void sauvegarde(){//sauvegarde l avancee (nombre de generations), la taille N de la matrice et la matrice.
     FILE * fichier;
-    fichier=fopen("sav.txt", "w");
-    fwrite(&gen, sizeof(int),1,fichier);
-    fwrite(&N, sizeof(int),1, fichier);
-    fwrite(mat, sizeof(case_t),N*N,fichier);
+    fichier=fopen("sav.txt", "w"); //fichier de sauvegarde
+    
+    fwrite(&gen, sizeof(int),1,fichier);//on prend gen
+    fwrite(&N, sizeof(int),1, fichier);//idem pour N
+    fwrite(mat, sizeof(case_t),N*N,fichier);//on recupere la matrice de la taille n*n
 
     fclose(fichier);
  }
 
-void charger(){
+void charger(){//charge la matrice de jeu, sa taille N et le nombre de generation (gen,n,mat)
     FILE * fichier;
     fichier=fopen("sav.txt", "r");
+    //on recupere les int
     fread(&gen, sizeof(int), 1, fichier);
-    fread(&N, sizeof(int), 1, fichier);
+    fread(&N, sizeof(int), 1, fichier)
+    //on enleve l allocatiln obsolete de la matrice, on l alloc a la bonne taille, et on y met les donnees
     free(matrice);
     matrice=malloc(sizeof(case_t)*N*N);
     fread(matrice, sizeof(case_t),N*N,fichier);
+    
     fclose(fichier);
 }
 
-void main(){
+void main(){ // pour tester les fonctiins, a supprimer quand vérifié
     matrice=malloc(sizeof(case_t)*N*N);
 
     int i,j;
