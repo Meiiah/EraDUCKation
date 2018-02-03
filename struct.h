c7cb865e9637f04ac6dd37f4593262cb35f8d01c
MaximeTouze <35963692+MaximeTouze@users.noreply.github.com>
2018-02-03 19:17:37 +0100

    Update struct.h
    ajout de présence nourriture sur le type case

--- struct.h
+++ struct.h
@@ -1,3 +1,4 @@
+
 #define nb_max 10
 
 typedef struct mur_s {
@@ -16,5 +17,6 @@ typedef struct case_s{
 	mur_t mur;
 	canard_t tab_canard[nb_max];
 	int nb_occupant; 
+	int pres_nourriture; //indique si il y a de la nouriture sur la case ou non, 0 nin 1 oui 
 }case_t
 
