43c5cd48f4d1d59c3c83385fa8c90e2eab42a16e
marion philippe <l2info030@ic2s114-07.univ-lemans.fr>
2018-02-13 16:00:49 +0100

    partie solo

--- jeu_solo.c
+++ jeu_solo.c
@@ -1,9 +1,20 @@
+#include "piege.h"
+#include "nourriture.h"
+#include "joueur.h"
+#include "struct.h"
+
 int jeu_solo(){
-	int jeu=-1 // -1 en cours de partie , 1 partie gagnée , 0 partie perdue
+	int jeu=1 // 1 en cours de partie , 0 partie finit
 	int nb_gen=0; // Compteur de génération
-		
-	while (jeu==-1){
-		
+	
+
+	while ( nb_gen<100 && presence_canard()==1){ // fin du jeu
+		spawn_nourriture();
+		piege();
+		deplacer();
+		choix_joueur();
+		ajout_score(100,joueur,NULL);
 		nb_gen++;
+		printf("Score: %i",joueur.score);	
 	}
 }
