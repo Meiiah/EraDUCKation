e5416f0fb8d5c92fa3e597c8bac8d03044cffeb3
Philippe Marion <l2info030@ic2s120-04.univ-lemans.fr>
2018-02-20 16:31:28 +0100

    joueur

--- joueur.h
+++ joueur.h
@@ -1,4 +1,7 @@
+void ajout_score(int point,joueur_t joueur, joueur_t joueur2);
 void caract_joueur(char joueur[25]);
-void tab_event_mauvai();
+void tab_event_mauvais();
 void tab_event_bon();
+void choix_mechant();
+void choix_bon();
 void choix_joueur();
