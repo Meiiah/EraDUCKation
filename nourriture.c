64c490281076b8ff31070a7b2aa43903e3a9d423
Philippe Marion <l2info030@ic2s120-04.univ-lemans.fr>
2018-02-20 16:31:56 +0100

    nourriture

--- nourriture.c
+++ nourriture.c
@@ -1,6 +1,6 @@
-void spawn_nourriture(){
+void spawn_nourriture(int nourriture_genere){
 	int i,k;
-	int random_nourriture;	
+		
 	int random_x;
 	int random_y;
 	int random_nbre_apparition=rand()%5;//random pour le nombre d'appartion de nourriture
@@ -8,8 +8,8 @@ void spawn_nourriture(){
 		random_x=rand_map();
 		random_y=rand_map();
 		//spawn de nourriture
-		random_nourriture=rand()%100;
-		matrice[i][j].pres_nourriture=random_nourriture;
+		
+		matrice[i][j].pres_nourriture=nourriture_genere;
 			
 	}
 }
