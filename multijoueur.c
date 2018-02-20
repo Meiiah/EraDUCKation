2c618f613d54baae5eaf20a887dedec4c4c7d7b6
MaximeTouze <35963692+MaximeTouze@users.noreply.github.com>
2018-02-20 16:04:42 +0100

    Doc

--- multijoueur.c
+++ multijoueur.c
@@ -22,21 +22,16 @@ typedef struct joueurm_s{
 
 case_t matrice[5][5];
 
-void choix_bon(void);
-void choix_mechant(void);
-void demander_nom(char *,char * );
-
-
 /*================================================*/
-
-joueur_multi_t joueur_gentil(void){ // initialise un joueur en joueur gentil
+/** \fn joueur_multi_t joueur_gentil(void)*/
+joueur_multi_t joueur_gentil(void){ /** initialise un joueur en joueur gentil*/
     joueur_multi_t mem;
     mem.clan = bon;
     mem.choix = choix_bon;
     return mem ;
 }
-
-joueur_multi_t joueur_mechant(void){ //initialise un joueur en tant que mechant
+/** \fn joueur_multi_t joueur_mechant(void)*/
+joueur_multi_t joueur_mechant(void){ /**initialise un joueur en tant que mechant*/
     joueur_multi_t mem;
     mem.clan = mechant;
     mem.choix = choix_mechant;
@@ -51,8 +46,8 @@ void tour_multijoueur(joueur_multi_t tab[]){
 }
 
 /*===============================================*/
-
-  int qui_commence(void){
+/** \fn int qui_commence(void) */
+int qui_commence(void){/** demande quel joueur commence */
   	  int tampon;
        printf("Si vous voulez que ce soit le joueur gentil qui commence, tapez 0, pour le mechant,         tapez 1, et si vous voulez que ce soit aleatoire tapez 2\n");
        scanf("%i",&tampon);
@@ -61,12 +56,16 @@ void tour_multijoueur(joueur_multi_t tab[]){
             scanf("%i",&tampon);
         }
         if(tampon ==2) tampon = rand()%2;
-        return tampon;
+        return tampon; /** \return renvoit 0 si c est le gentil et 1 si c est le mechant*/
 }
 
 /*============================================================*/
+/** \fn void init_tab_joueurs(joueur_multi_t * tab, int tampon) */
+/** met le premier joueur a jouer en premiere place dans le tableau */
 
-void init_tab_joueurs(joueur_multi_t * tab, int tampon){
+void init_tab_joueurs(joueur_multi_t * tab /** tableau de joueurs */,
+						int tampon /** resultat du placement */){ 
+
     tab[tampon]= joueur_gentil();    //c est de la logique
     tab[tampon +1 %2] = joueur_mechant();
 
@@ -75,8 +74,8 @@ void init_tab_joueurs(joueur_multi_t * tab, int tampon){
     demander_nom(tab[tampon].nom, tab[tampon+1 %2].nom);
 }
 /*============================================================*/
-
-void main_multijoueur(void){
+/** \fn void main_multijoueur(void) */
+void main_multijoueur(void){ /** fait le jeu en multi sur meme pc */
     //variables :
     joueur_multi_t tab[2];
     int tampon;
@@ -84,9 +83,9 @@ void main_multijoueur(void){
     tampon = qui_commence();
     init_tab_joueurs(tab, tampon);
 
-    /** Tant que les canards sont pas tous morts on fait les tours
+    /* Tant que les canards sont pas tous morts on fait les tours
 
-    a chaque tour on fait deplacement, choix, on verifie les cond d arret,, et on recommence */
+    a chaque tour on fait deplacement, choix, on verifie les cond d arret, et on recommence */
 
 
 }
