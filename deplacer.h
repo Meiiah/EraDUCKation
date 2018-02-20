e902c423cc42c0ef1c425d90b48fd9af9ab0fd32
Philippe Marion <l2info030@ic2s120-04.univ-lemans.fr>
2018-02-20 16:02:57 +0100

    fichier .h de deplacer.c

--- deplacer.h
+++ deplacer.h
@@ -1,1 +1,9 @@
+canard_t canard_vide(void);
+
+int voit_nourriture(int i,int j,int k);
+
+int voit_accouplement(int i,int j,int k);
+
+void deplacer_canard(int i, int j, int k, int direction);
+
 void deplacer(void);
