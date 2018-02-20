1b3e2a1bb07f2d69e39b4a8b21c75589a72f6c92
Philippe Marion <l2info030@ic2s120-04.univ-lemans.fr>
2018-02-20 16:31:14 +0100

    jeu solo

--- jeu_solo.c
+++ jeu_solo.c
@@ -7,10 +7,16 @@
 int jeu_solo(){
 	int jeu=1 // 1 en cours de partie , 0 partie finit
 	int nb_gen=0; // Compteur de génération
-	
+	int nourriture_genere;
+	int famine=0;
 
 	while ( nb_gen<100 && presence_canard()==1){ // fin du jeu
-		spawn_nourriture();
+		if(famine){
+			nourriture_genere=rand()%100;
+		}else{
+			nourriture_genere=0;
+		}
+		spawn_nourriture(nourriture_genere);
 		piege();
 		deplacer();
 		choix_joueur();
