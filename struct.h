8edce51a455805e4bb5966dbfe1368fe75fc9efe
VAIDIE camille <camille.vaidie@gmail.com>
2018-02-20 16:07:21 +0100

    doc

--- struct.h
+++ struct.h
@@ -1,6 +1,19 @@
 
+/**
+*\file struct.h
+*\brief structure
+*\author VAIDIE Camille
+*\version 1.0
+*\date 20 fevrier 2018
+*/
+
+
+
 #define nb_max 10
 int nb_gen;
+
+/** \struct mur_s struct.h*/
+/** etat du mur*/
 typedef struct mur_s {
 	int murN;	//mur :1 , pas mur :0//
 	int murS;
@@ -8,11 +21,15 @@ typedef struct mur_s {
 	int murO;
 }mur_t;
 
+/**\struct canard_s struct.h*/
+/** etat des canards*/
 typedef struct canard_s {
 	int nourriture;	// /!\ POURCENTAGE//
 	int etat; // 0: oeuf,1:adulte, -1:inexistant//
 }canard_t;
-	
+
+/**struct case_s struct.h*/
+/** etat des cases	*/
 typedef struct case_s{
 	mur_t mur; // Contient les 4murs : Nord,Est,Ouest,Sud d'une case//
 	canard_t tab_canard[nb_max];
@@ -21,6 +38,8 @@ typedef struct case_s{
 	int pres_piege;
 }case_t;
 
+/**\struct joueur_s struct.h*/
+/**brief information joueur*/
 typedef struct joueur_s{
 	int score;
 	char nom_joueur[25];
