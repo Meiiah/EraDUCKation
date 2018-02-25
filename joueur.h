113a569b74cc4997f5e985e5a110d070b8a90364
Meiiah <marion.philippe72@gmail.com>
2018-02-25 19:51:07 +0100

    fonctions joueur de camille -h

---
+++ joueur.h
@@ -0,0 +1,7 @@
+void ajout_score(int,joueur_t, joueur_t);
+void caract_joueur(joueur_t);
+void tab_event_mauvais(int,int,int);
+void tab_event_bon(int,int,int);
+void choix_mechant();
+void choix_bon();
+void choix_joueur();
