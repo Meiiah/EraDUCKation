44145386d3282c899e78fc8db76d020984e92571
Meiiah <marion.philippe72@gmail.com>
2018-02-25 19:37:51 +0100

    Fonction de piege

--- piege.c
+++ piege.c
@@ -1,17 +1,17 @@
+#include <stdlib.h>
 #include "struct.h"
 #include <stdio.h>
 #define taille_mat 20 //taille de la matrice//
 case_t matrice[taille_mat][taille_mat];
 
 void piege(){ //fonction piege
-	int i;
-	int j;
 	int x=(rand() % (taille_mat)); //place en random un piege dans la matrice
 	int y=(rand() % (taille_mat));
 	matrice[x][y].pres_piege=1; // 1: presence piege , 0: pas de piege
 }
 
 void presence_piege(){ // fonction presence de piege
+	int i,j;
 	for(i=0;i<taille_mat;i++){ //parcour de la matrice
 		for(j=0;j<taille_mat;j++){
 			if(matrice[i][j].pres_piege==1 && matrice[i][j].nb_occupant>0){ // 1: piege present)
