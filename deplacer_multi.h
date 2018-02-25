167d1ffc827e2000927ecb7e8ae93d391317f951
Meiiah <marion.philippe72@gmail.com>
2018-02-25 19:31:09 +0100

    fonctions de deplacement en multi

--- deplacer_multi.h
+++ deplacer_multi.h
@@ -1,3 +1,3 @@
-void deplacer_multi_serveur(int socket_to_client);
+void deplacer_multi_serveur(case_t ** matrice,int taille_mat,int nourriture_accouplement,int socket_to_client);
 
 void deplacer_multi_client(int socket_to_serv);
