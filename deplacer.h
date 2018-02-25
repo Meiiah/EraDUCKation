32011fecf24174b2261bbf9227ab8012c84b7b69
Meiiah <marion.philippe72@gmail.com>
2018-02-25 19:30:51 +0100

    fonctions de deplacement .h

--- deplacer.h
+++ deplacer.h
@@ -1,9 +1,9 @@
 canard_t canard_vide(void);
 
-int voit_nourriture(int i,int j,int k);
+int voit_nourriture(case_t ** matrice,int i,int j,int k);
 
-int voit_accouplement(int i,int j,int k);
+int voit_accouplement(case_t ** matrice,int nourriture_accouplement,int i,int j,int k);
 
-void deplacer_canard(int i, int j, int k, int direction);
+void deplacer_canard(case_t ** matrice,int i, int j, int k, int direction);
 
-void deplacer(void);
+void deplacer(case_t ** matrice,int nourriture_accouplement);
