4c4c98d66d4afb36cdf5f8fa55d44f27212c4a44
Meiiah <marion.philippe72@gmail.com>
2018-02-25 19:38:22 +0100

    Fonction de sauvegarde

--- sauvegarde.c
+++ sauvegarde.c
@@ -1,28 +1,27 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include "struct.h"
 
-int N =10;
 
-case_t* matrice;
 int gen = 12;
 
-void sauvegarde(){//sauvegarde l avancee (nombre de generations), la taille N de la matrice et la matrice.
+void sauvegarde(case_t **matrice,int N){//sauvegarde l avancee (nombre de generations), la taille N de la matrice et la matrice.
     FILE * fichier;
     fichier=fopen("sav.txt", "w"); //fichier de sauvegarde
     
     fwrite(&gen, sizeof(int),1,fichier);//on prend gen
     fwrite(&N, sizeof(int),1, fichier);//idem pour N
-    fwrite(mat, sizeof(case_t),N*N,fichier);//on recupere la matrice de la taille n*n
+    fwrite(matrice, sizeof(case_t),N*N,fichier);//on recupere la matrice de la taille n*n
 
     fclose(fichier);
  }
 
-void charger(){//charge la matrice de jeu, sa taille N et le nombre de generation (gen,n,mat)
+void charger(case_t ** matrice, int N){//charge la matrice de jeu, sa taille N et le nombre de generation (gen,n,mat)
     FILE * fichier;
     fichier=fopen("sav.txt", "r");
     //on recupere les int
     fread(&gen, sizeof(int), 1, fichier);
-    fread(&N, sizeof(int), 1, fichier)
+    fread(&N, sizeof(int), 1, fichier);
     //on enleve l allocatiln obsolete de la matrice, on l alloc a la bonne taille, et on y met les donnees
     free(matrice);
     matrice=malloc(sizeof(case_t)*N*N);
@@ -30,7 +29,7 @@ void charger(){//charge la matrice de jeu, sa taille N et le nombre de generatio
     
     fclose(fichier);
 }
-
+/*
 void main(){ // pour tester les fonctiins, a supprimer quand vérifié
     matrice=malloc(sizeof(case_t)*N*N);
 
@@ -68,3 +67,4 @@ void main(){ // pour tester les fonctiins, a supprimer quand vérifié
     printf("gen:%i", gen);
 
 }
+*/
