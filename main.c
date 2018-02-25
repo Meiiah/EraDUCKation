d5479b5f9e8043b2ca884a0b3b6e4aa27a012493
Meiiah <marion.philippe72@gmail.com>
2018-02-25 19:36:02 +0100

    main

--- main.c
+++ main.c
@@ -1,13 +1,19 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#include "struct.h"
+#include "event.h"
 #include "joueur.h"
 #include "canard.h"
-#include "struct.h"
 #include "labyrinthe.h"
+#include "deplacer_multi.h"
+#include "init_matrice.h"
+#include "jeu_solo.h"
+#include "matrice.h"
 
 
 int choix_difficultee(int choix){
+	int taille_mat;
 	switch(choix){
 		case 1 :
 			return taille_mat=15;
@@ -20,11 +26,12 @@ int choix_difficultee(int choix){
 		break;		
 	}
 }
-int choix_nbr_joueur(int choix){
+int choix_nbr_joueur(int choix,joueur_t joueur,joueur_t joueur2){
 	int nbr_joueur;
 	switch(choix){
 		case 1 :
 			caract_joueur(joueur);
+			joueur2.score=0;
 			return nbr_joueur=1;
 		break;	
 		case 2:
@@ -37,6 +44,22 @@ int choix_nbr_joueur(int choix){
 
 
 int main(){
+
+	//definition matrice
+	int taille_mat;
+	case_t ** matrice;
+
+	//definition joueur
+	joueur_t joueur;
+	joueur_t joueur2;
+	joueur2.nom_joueur[0]='n';
+	joueur2.nom_joueur[1]='u';
+	joueur2.nom_joueur[2]='l';
+	joueur2.nom_joueur[3]='l';
+
+	//definition nourriture
+	int nourriture_accouplement;//Nourriture qu'on a besoin pour se reproduire
+	int nourriture_genere;//Nourriture qui apparait
 	
 	int choix; // Choix du joueur
 	int nbr_joueur;
@@ -62,21 +85,22 @@ int main(){
 			printf("1 : 1 joueur\n");
 			printf("2 : 2 joueurs\n");
 			scanf("%i",&choix);
-			nbr_joueur=choix_nbr_joueur(choix);
+			nbr_joueur=choix_nbr_joueur(choix,joueur,joueur2);
 			
 			/*Mise en place de la matrice adaptée*/
-			init_matrice(taille_mat);
+			creation_matrice(matrice,taille_mat);
+			init_matrice(matrice,taille_mat);
 
 			/*Creation du Labyrinthe*/
 			creer_labyrinthe();
 			
 			//Apparition de canard
-			init_canard(); 
+			init_canard(matrice,taille_mat); 
 
 			if(nbr_joueur==1){
-				jeu_solo();
+				jeu_solo(matrice,taille_mat,nourriture_genere,nourriture_accouplement,joueur,joueur2);
 			}else{
-				jeu_multi();
+				//jeu_multi();
 			}
 			
 		break;
