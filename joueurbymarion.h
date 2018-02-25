5bf8e6d54e1cd802f67cb07ab380f45a1a6f52c3
Meiiah <marion.philippe72@gmail.com>
2018-02-25 19:29:29 +0100

    joueur.h modifié par marion

---
+++ joueurbymarion.h
@@ -0,0 +1,7 @@
+
+void ajout_score(int,joueur_t, joueur_t);
+void caract_joueur(joueur_t);
+
+void choix_mechant(case_t ** matrice,int taille_mat,int nourriture_accouplement,int nourriture_genere,joueur_t joueur, joueur_t joueur2);
+void choix_bon(case_t ** matrice,int taille_mat,int nourriture_accouplement,int nourriture_genere,joueur_t joueur, joueur_t joueur2);
+void choix_joueur(case_t ** matrice,int taille_mat,int nourriture_accouplement,int nourriture_genere,joueur_t joueur, joueur_t joueur2);
