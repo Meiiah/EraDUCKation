1f5fc343d45ac936cc73f9fc9bd606f4685f8e4d
Meiiah <marion.philippe72@gmail.com>
2018-02-25 19:34:53 +0100

    fonctions joueurs -h

--- joueur.h
+++
@@ -1,8 +0,0 @@
-
-void ajout_score(int,joueur_t, joueur_t);
-void caract_joueur(joueur_t);
-void tab_event_mauvais(int,int,int);
-void tab_event_bon(int,int,int);
-void choix_mechant();
-void choix_bon();
-void choix_joueur();
