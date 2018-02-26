7aba0319cde99ab3c477bc208e9a2bf2d8fb019a
Meiiah <marion.philippe72@gmail.com>
2018-02-27 00:04:56 +0100

    module mourriture

--- nourriture.c
+++ nourriture.c
@@ -1,19 +1,19 @@
 #include <stdlib.h>
 #include "struct.h"
+#include "matrice.h"
 #include "outils.h"
 
-void spawn_nourriture(case_t ** matrice,int taille_mat,int nourriture_genere){
+void spawn_nourriture(caract_mat_t * cmat,int nourriture_genere){
 	int i,k;
 		
 	int random_x;
 	int random_y;
 	int random_nbre_apparition=rand()%5;//random pour le nombre d'appartion de nourriture
 	for(i=0;i<random_nbre_apparition;i++){ 
-		random_x=rand_map(taille_mat);
-		random_y=rand_map(taille_mat);
-		//spawn de nourriture
+		random_x=rand_map(cmat->taille_mat_x);
+		random_y=rand_map(cmat->taille_mat_y);
 		
-		matrice[random_x][random_y].pres_nourriture=nourriture_genere;
-			
+		//spawn de nourriture
+		cmat->matrice[random_x][random_y].pres_nourriture=nourriture_genere;
 	}
 }
