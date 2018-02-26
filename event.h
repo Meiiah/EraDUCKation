a5d47dc0f546e4e6c99d9554aec046d56817e305
Meiiah <marion.philippe72@gmail.com>
2018-02-27 00:01:17 +0100

    fonction evenemnt -h

--- event.h
+++ event.h
@@ -1,17 +1,18 @@
+#define M 10
 
 int (*mauvais[M])(void);
 int (*bon[M])(void);
 
 /* Evenement mauvais */
-int tsunami(case_t ** matrice, int taille_mat, joueur_t joueur, joueur_t joueur2);
+int tsunami(caract_mat_t * cmat, joueur_t joueur, joueur_t joueur2);
 
-int tempete(case_t ** matrice, int taille_mat, joueur_t joueur, joueur_t joueur2);
+int tempete(caract_mat_t * cmat, joueur_t joueur, joueur_t joueur2);
 
 int famine(int nourriture_genere);
 
 int reproduction_ralentie(int nourriture_accouplement);
 
-int apparition_predateur(case_t ** matrice, int taille_mat, joueur_t joueur, joueur_t joueur2);
+int apparition_predateur(caract_mat_t * cmat, joueur_t joueur, joueur_t joueur2);
 
 /* Evenement bon */
 
@@ -19,14 +20,12 @@ int reproduction_acceleree(int nourriture_accouplement);
 
 int plus_nourriture(int nourriture_genere);
 
-int joker_nourriture(case_t ** matrice,int taille_mat);
+int joker_nourriture(caract_mat_t * cmat);
 
-int liberation_canard(case_t ** matrice, int taille_mat, joueur_t joueur, joueur_t joueur2); //ajout au score
+int liberation_canard(caract_mat_t * cmat, joueur_t joueur, joueur_t joueur2); //ajout au score
 
 int canard_invincible();
 
-/*
-int init_tab_mauvais();
+int init_tab_event_mauvais();
 
-int init_tab_bon();
-*/
+int init_tab_event_bon();
