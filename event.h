61287e86447a1e05d18abf612746e60659a33208
camvde <35963489+camvde@users.noreply.github.com>
2018-02-26 10:44:50 +0100

    Update event.h

--- event.h
+++ event.h
@@ -1,14 +1,17 @@
 
-/* Evenement mauvais */
-void tsunami(case_t ** matrice, int taille_mat, joueur_t joueur, joueur_t joueur2);
+int (*mauvais[M])(void);
+int (*bon[M])(void);
 
-void tempete(case_t ** matrice, int taille_mat, joueur_t joueur, joueur_t joueur2);
+/* Evenement mauvais */
+int tsunami(case_t ** matrice, int taille_mat, joueur_t joueur, joueur_t joueur2);
+
+int tempete(case_t ** matrice, int taille_mat, joueur_t joueur, joueur_t joueur2);
 
 int famine(int nourriture_genere);
 
 int reproduction_ralentie(int nourriture_accouplement);
 
-void apparition_predateur(case_t ** matrice, int taille_mat, joueur_t joueur, joueur_t joueur2);
+int apparition_predateur(case_t ** matrice, int taille_mat, joueur_t joueur, joueur_t joueur2);
 
 /* Evenement bon */
 
@@ -16,10 +19,14 @@ int reproduction_acceleree(int nourriture_accouplement);
 
 int plus_nourriture(int nourriture_genere);
 
-void joker_nourriture(case_t ** matrice,int taille_mat);
+int joker_nourriture(case_t ** matrice,int taille_mat);
 
-void liberation_canard(case_t ** matrice, int taille_mat, joueur_t joueur, joueur_t joueur2); //ajout au score
+int liberation_canard(case_t ** matrice, int taille_mat, joueur_t joueur, joueur_t joueur2); //ajout au score
 
-void canard_invincible();
+int canard_invincible();
 
+/*
+int init_tab_mauvais();
 
+int init_tab_bon();
+*/
