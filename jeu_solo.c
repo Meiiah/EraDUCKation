b12cf2dafabd6c7f0a4079a42a17bc5747572b24
Meiiah <marion.philippe72@gmail.com>
2018-02-27 00:02:26 +0100

    jeu solo

--- jeu_solo.c
+++ jeu_solo.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
-
 #include "struct.h"
+#include "matrice.h"
 #include "piege.h"
 #include "nourriture.h"
 #include "joueur.h"
@@ -8,18 +8,17 @@
 #include "deplacer.h"
 #include "deplacer_multi.h"
 
-int jeu_solo(case_t ** matrice,int taille_mat,int nourriture_genere,int nourriture_accouplement,joueur_t joueur,joueur_t joueur2){
+int jeu_solo(caract_mat_t * cmat,int nourriture_genere,int nourriture_accouplement,joueur_t joueur,joueur_t joueur2){
 	int jeu=1; // 1 en cours de partie , 0 partie finit
 	int nb_gen=0; // Compteur de génération
 
-	while ( nb_gen<100 && presence_canard(matrice,taille_mat)==1){ // fin du jeu
-
-		spawn_nourriture(matrice,taille_mat,nourriture_genere);
-		piege();
-		deplacer(matrice,nourriture_accouplement);
-		choix_joueur(matrice,taille_mat,nourriture_accouplement,nourriture_genere,joueur,joueur2);
+	while ( nb_gen<100 && presence_canard(cmat)==1){ // fin du jeu
+		spawn_nourriture(cmat,nourriture_genere);
+		piege(cmat);
+		deplacer(cmat,nourriture_accouplement);
+		choix_joueur(cmat,nourriture_accouplement,nourriture_genere,joueur,joueur2);
 		ajout_score(100,joueur,joueur2);
 		nb_gen++;
-		printf("Score: %i",joueur.score);	
+		printf("Score: %i",joueur.score);
 	}
 }
