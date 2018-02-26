2b345efb677cae560879fa8b469b53586eb27da6
Meiiah <marion.philippe72@gmail.com>
2018-02-27 00:04:45 +0100

    module matrice-h

--- matrice.h
+++ matrice.h
@@ -1,3 +1,13 @@
+/*definition de la matrice */
+typedef struct{
+	case_t ** matrice;
+	int taille_mat_x;
+	int taille_mat_y;
+}caract_mat_t;
 
+int est_dans_matrice(caract_mat_t *cmat,int x, int y);
 
-void creation_matrice(case_t ** matrice, int taille_mat);
+void creation_matrice(caract_mat_t * cmat);
+
+void init_matrice(caract_mat_t * cmat);
+
