1b1c029f9c7c48771ec95b1936f8243498c0a6d1
Meiiah <marion.philippe72@gmail.com>
2018-02-27 00:00:47 +0100

    fonction deplacé_multi -h

--- deplacer_multi.h
+++ deplacer_multi.h
@@ -1,8 +1,8 @@
 #ifndef __DEPLACER_MULTI_H__
 #define __DEPLACER_MULTI_H__
 
-void deplacer_multi_serveur(case_t ** matrice,int taille_mat,int nourriture_accouplement,int socket_to_client);
+void deplacer_multi_serveur(caract_mat_t * cmat,int nourriture_accouplement,int socket_to_client);
 
-void deplacer_multi_client(int socket_to_serv);
+void deplacer_multi_client(caract_mat_t * cmat,int socket_to_serv);
 
 #endif // __DEPLACER_MULTI_H__
