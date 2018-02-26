bdeabc8945375ef18b6b703e40f598adfa4e1f3a
Meiiah <marion.philippe72@gmail.com>
2018-02-27 00:05:15 +0100

    module piege

--- piege.c
+++ piege.c
@@ -1,25 +1,33 @@
 #include <stdlib.h>
-#include "struct.h"
 #include <stdio.h>
+#include "struct.h"
+#include "matrice.h"
 
-
-void piege(case_t ** matrice, int taille_mat){ //fonction piege
-	int x=(rand() % (taille_mat)); //place en random un piege dans la matrice
-	int y=(rand() % (taille_mat));
-	matrice[x][y].pres_piege=1; // 1: presence piege , 0: pas de piege
+void piege(caract_mat_t * cmat){ //fonction piege
+	int x=(rand() % (cmat->taille_mat_x)); //place en random un piege dans la matrice
+	int y=(rand() % (cmat->taille_mat_y));
+	if(est_dans_matrice(cmat,x,y)){
+		cmat->matrice[x][y].pres_piege=1; // 1: presence piege , 0: pas de piege
+	}else{
+		printf("ERREUR - sortie de matrice : fonction piege dans piege.c");
+	}
 }
 
-void presence_piege(case_t ** matrice, int taille_mat){ // fonction presence de piege
+void presence_piege(caract_mat_t * cmat){ // fonction presence de piege
 	int i,j;
-	for(i=0;i<taille_mat;i++){ //parcour de la matrice
-		for(j=0;j<taille_mat;j++){
-			if(matrice[i][j].pres_piege==1 && matrice[i][j].nb_occupant>0){ // 1: piege present)
-				for(int k=0; k<matrice[i][j].nb_occupant;k++){
-					matrice[i][j].tab_canard[k].nourriture=0;
-					matrice[i][j].tab_canard[k].etat=-1;
+	for(i=0;i<cmat->taille_mat_x;i++){ //parcour de la matrice
+		for(j=0;j<cmat->taille_mat_y;j++){
+			if(est_dans_matrice(cmat,i,j)){
+				if(cmat->matrice[i][j].pres_piege==1 && cmat->matrice[i][j].nb_occupant>0){ // 1: piege present)
+					for(int k=0; k<cmat->matrice[i][j].nb_occupant;k++){
+						cmat->matrice[i][j].tab_canard[k].nourriture=0;
+						cmat->matrice[i][j].tab_canard[k].etat=-1;
+					}
+					cmat->matrice[i][j].pres_piege=0;
+					cmat->matrice[i][j].nb_occupant=0; // Mort des canards sur cette case
 				}
-				matrice[i][j].pres_piege=0;
-				matrice[i][j].nb_occupant=0; // Mort des canards sur cette case
+			}else{
+				printf("ERREUR - sortie de matrice : fonction presence_piege dans piege.c");
 			}
 		}
 	}
