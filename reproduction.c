18c254e822955b3cb38c23b130950b23383eca32
MaximeTouze <35963692+MaximeTouze@users.noreply.github.com>
2018-03-22 16:45:31 +0100

    Add files via upload

--- reproduction.c
+++ reproduction.c
@@ -12,37 +12,61 @@
 */ 
 
 int ou_pondre(caract_mat_t * cmat, int * i, int * j){
+	
 	int cpt =1;
-	while(1){
-		if(cmat->matrice[*i][*j].nb_occupant>10){
-			if(est_dans_matrice(cmat,*i-cpt,*j) && cmat->matrice[*i-cpt][*j].nb_occupant<10){
-				*i=*i-cpt;
-				return 1;
-			}else if(est_dans_matrice(cmat,*i-cpt,*j-cpt) && cmat->matrice[*i-cpt][*j-cpt].nb_occupant<10){
-				*i=*i-cpt;
-				*j=*j-cpt;
-				return 1;
-			}else if(est_dans_matrice(cmat,*i,*j-cpt) && cmat->matrice[*i][*j-cpt].nb_occupant<10){
-				*j=*j-cpt;
-				return 1;
-			}else if(est_dans_matrice(cmat,*i+cpt,*j-cpt) && cmat->matrice[*i+cpt][*j-cpt].nb_occupant<10){
-				*i=*i+cpt;
-				*j=*j-cpt;
-				return 1;
-			}else if(est_dans_matrice(cmat,*i+cpt,*j) && cmat->matrice[*i+cpt][*j].nb_occupant<10){
-				*i=*i+cpt;
-				return 1;
-			}else if(est_dans_matrice(cmat,*i+cpt,*j+cpt) && cmat->matrice[*i+cpt][*j+cpt].nb_occupant<10){
-				*i=*i+cpt;
-				*j=*j+cpt;
-				return 1;
-			}else if(est_dans_matrice(cmat,*i,*j+cpt) && cmat->matrice[*i][*j+cpt].nb_occupant<10){
-				*j=*j+cpt;
-				return 1;
-			}else if(est_dans_matrice(cmat,*i-cpt,*j+cpt) && cmat->matrice[*i-cpt][*j+cpt].nb_occupant<10){
-				*i=*i-cpt;
-				*j=*j+cpt;
-				return 1;
+	while(1){ 
+		if(cmat->matrice[*i][*j].nb_occupant>8){ // Vu qu on ajoute un canard et qu on a 9 places il faut qu il y ait maximum 8 occupants
+			if(est_dans_matrice(cmat,*i-cpt,*j)){
+				if( cmat->matrice[*i-cpt][*j].nb_occupant<9){
+					*i=*i-cpt;
+					return 1;
+				}
+fprintf(stderr,"\n1\n");
+			}else if(est_dans_matrice(cmat,*i-cpt,*j-cpt)){
+				if(cmat->matrice[*i-cpt][*j-cpt].nb_occupant<9){
+					*i=*i-cpt;
+					*j=*j-cpt;
+					return 1;
+				}
+fprintf(stderr,"\n2\n");
+			}else if(est_dans_matrice(cmat,*i,*j-cpt)){
+				if( cmat->matrice[*i][*j-cpt].nb_occupant<9){
+					*j=*j-cpt;
+					return 1;
+				}
+fprintf(stderr,"\n3\n");
+			}else if(est_dans_matrice(cmat,*i+cpt,*j-cpt) ){
+				if( cmat->matrice[*i+cpt][*j-cpt].nb_occupant<9){
+					*i=*i+cpt;
+					*j=*j-cpt;
+					return 1;
+				}
+fprintf(stderr,"\n4\n");
+			}else if(est_dans_matrice(cmat,*i+cpt,*j) ){
+				if( cmat->matrice[*i+cpt][*j].nb_occupant<9){
+					*i=*i+cpt;
+					return 1;
+				}
+fprintf(stderr,"\n5\n");
+			}else if(est_dans_matrice(cmat,*i+cpt,*j+cpt) ){
+				if( cmat->matrice[*i+cpt][*j+cpt].nb_occupant<9){
+					*i=*i+cpt;
+					*j=*j+cpt;
+					return 1;
+				}
+fprintf(stderr,"\n6\n");
+			}else if(est_dans_matrice(cmat,*i,*j+cpt) ){
+				if( cmat->matrice[*i][*j+cpt].nb_occupant<9){
+					*j=*j+cpt;
+					return 1;
+				}
+fprintf(stderr,"\n7\n");
+			}else if(est_dans_matrice(cmat,*i-cpt,*j+cpt) ){
+				if( cmat->matrice[*i-cpt][*j+cpt].nb_occupant<9){
+					*i=*i-cpt;
+					*j=*j+cpt;
+					return 1;
+				}fprintf(stderr,"\n8\n");
 			}
 		
 		}else{
@@ -61,27 +85,35 @@ void reproduction(caract_mat_t * mat, int nourriture_accouplement,joueur_t joueu
 	int compteur;
 	for(i=0;i<mat->taille_mat_x;i++){
 		for(j=0;j<mat->taille_mat_y;j++){
+			fprintf(stderr,"\nDEB case x: %i   y : %i\n",i, j);
+
 			//Parcours du nombre de canard//
 			compteur=0;//compteur du nombre de canard favorable a la reproduction//
 			for(k=0;k<nb_max;k++){
 				if(mat->matrice[i][j].tab_canard[k].nourriture>=nourriture_accouplement && mat->matrice[i][j].tab_canard[k].etat==1 ){
-					int rande=rand()%5;
-					if(rande==0 || rande==1){
+					//Si un canard adulte peut se reproduire
+					int rande=rand()%5; 
+					if(rande<=1){// il a 20%de chance de le faire ( 0 ou 1)
 						compteur++;
 					}
 				}
 			}
 			
-			compteur/=2;
-			int x=i, y=j;
-			for(l=0;l<compteur;l++){
-				ou_pondre(mat, &x, &y);
+			compteur/=2;// On compte le nombre de bébé canards à créer
+			int x,y;
+			for(l=0;l<compteur;l++){//pour chaque canard à naitre a partir de cette case
+				x=i;
+				y=j;
+				fprintf(stderr,"\nDEB ou pondre x: %i   y : %i\n",i, j);
+				ou_pondre(mat, &x, &y);//on cherche où pondre
+				fprintf(stderr,"\nFIN ou pondre");
 
-				mat->matrice[x][y].nb_occupant++; //Ajout des nouveaux canards//
-				mat->matrice[x][y].tab_canard[l].etat=1;
-				mat->matrice[x][y].tab_canard[l].nourriture=50;
+				
+				mat->matrice[x][y].tab_canard[mat->matrice[x][y].nb_occupant].etat=0;
+				mat->matrice[x][y].tab_canard[mat->matrice[x][y].nb_occupant].nourriture=50;
+				mat->matrice[x][y].nb_occupant++; //Ajout du nouveau canard//
 				ajout_score(25,&joueur,&joueur2);
 			}
-		}
+		}fprintf(stderr,"\nFIN case x: %i   y : %i\n",i, j);
 	}
 }
