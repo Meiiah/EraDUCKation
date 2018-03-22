56b4a4557658133186747a92e0beef4422836181
MaximeTouze <35963692+MaximeTouze@users.noreply.github.com>
2018-03-22 14:49:52 +0100

    Add files via upload

--- joueur.h
+++ joueur.h
@@ -2,7 +2,7 @@
 #define __JOUEUR_H__
 
 
-void ajout_score(int,joueur_t, joueur_t);
+void ajout_score(int,joueur_t*, joueur_t*);
 void caract_joueur(joueur_t*);
 void tab_event_mauvais(int,int,int);
 void tab_event_bon(int,int,int);
