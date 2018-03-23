e8ba87a16f5c0c5906f434caa9e1f6cd7e8de6f6
Killian <l2info025@ic2s122-01.univ-lemans.fr>
2018-03-23 10:30:44 +0100

    enlevage des prints

--- deplacer.c
+++ deplacer.c
@@ -291,82 +291,80 @@ void deplacer(caract_mat_t * cmat, int nourriture_accouplement,int nourriture_ge
     int i,j,k;
     int direction=-1;
     int verif =1;
-	fprintf(stderr,"\nDEB DEB\n");
+	
     for(j=0; j< cmat->taille_mat_x ; j++){
     		for(i=0; i< cmat->taille_mat_y; i++){//pour chaque case de la matrice
         		for(k=0; k<cmat->matrice[i][j].nb_occupant; k++){
-					fprintf(stderr,"\nDEBUT POUR UN COINCOIN\n");
-            		if(cmat->matrice[i][j].tab_canard[k].etat==1){ //pour chaque canard qui peut se deplacer
-                                // /!\ 1/3 des canards se déplacent, donc rand, si c est pas bon on le deplace pas donc break
-                            if(rand()%3==0){
-                                //si il peut se deplacer on continue
-                                // si on voit un canard a accoupler on continue, sinon on continue les verifications
-										fprintf(stderr,"\nDEB voit_accouplement i:%i j:%i \n", i, j);
-				                        direction = voit_accouplement(cmat,nourriture_accouplement,i, j, k);
-				                        //fprintf(stderr,"\nFIN voit_accouplement\n");
+					
+		    		if(cmat->matrice[i][j].tab_canard[k].etat==1){ //pour chaque canard qui peut se deplacer
+		                        // /!\ 1/3 des canards se déplacent, donc rand, si c est pas bon on le deplace pas donc break
+		                    if(rand()%3==0){
+		                        //si il peut se deplacer on continue
+		                        // si on voit un canard a accoupler on continue, sinon on continue les verifications
+										
+				                direction = voit_accouplement(cmat,nourriture_accouplement,i, j, k);
+				                //fprintf(stderr,"\nFIN voit_accouplement\n");
 
-                            		if(direction==Aucune_direction){
-                                		//si il voit de la nourriture aller dessus sinon on deplace dabs une direction random arpres verif
-										//fprintf(stderr,"\nDEB voit_nourriture i:%i j:%i \n", i, j);
-                                		direction = voit_nourriture(cmat,i, j);
-										fprintf(stderr,"\nFIN voit_nourriture\n");
+		                    		if(direction==Aucune_direction){
+		                        		//si il voit de la nourriture aller dessus sinon on deplace dabs une direction random arpres verif
+											//fprintf(stderr,"\nDEB voit_nourriture i:%i j:%i \n", i, j);
+		                        		direction = voit_nourriture(cmat,i, j);
+										
+		                        		if(direction==Aucune_direction){
+		                        		//sinon se deplacer d une case vers la direction random apres verif
+				                    		do{
+									direction = rand()%4 +1;
+									/* FAIRE VERIF */
 
-                                		if(direction==Aucune_direction){
-                                		//sinon se deplacer d une case vers la direction random apres verif
-                                    		do{
-								                        		direction = rand()%4 +1;
-								                        		/* FAIRE VERIF */
+										switch(direction){// effets de bord
+											case Est:
+												if( i+1 < cmat->taille_mat_x){
+													//i croit et vu qu on est dans le tableau pas besoin de tout verifier
+													if(cmat->matrice[ i ][ j ].mur.murE !=1){
+														verif=1;
+													}
+												}
+											break;
 
-													 									switch(direction){// effets de bord
-																								case Est:
-																										if( i+1 < cmat->taille_mat_x){
-																												//i croit et vu qu on est dans le tableau pas besoin de tout verifier
-								                                        if(cmat->matrice[ i ][ j ].mur.murE !=1){
-								                                        		verif=1;
-								                                        }
-																										}
-																								break;
+											case Ouest:
+												if( i-1 >= 0){
+												//i decroit et vu qu on est dans le tableau pas besoin de tout verifier
+													if(cmat->matrice[ i ][ j ].mur.murO !=1)
+														verif=1;
+												}
+											break;
 
-																								case Ouest:
-																										if( i-1 >= 0){
-																												//i decroit et vu qu on est dans le tableau pas besoin de tout verifier
-																												if(cmat->matrice[ i ][ j ].mur.murO !=1)
-								                                      			verif=1;
-																										}
-				                                        break;
+											// idem avec j
+											case Sud:
+												if( j+1 < cmat->taille_mat_y ){
+													if(cmat->matrice[ i ][ j ].mur.murS !=1 )
+														verif=1;
+												}
+											break;
 
-																					 			// idem avec j
-																								case Sud:
-																										if( j+1 < cmat->taille_mat_y ){
-																												if(cmat->matrice[ i ][ j ].mur.murS !=1 )
-								                                            verif=1;
-																										}
-				                                        break;
+											case Nord:
+												if( j-1 >=0){
+													if(cmat->matrice[ i ][ j ].mur.murN !=1 )
+														verif=1;
+												}
+											break;
+										}
 
-																								case Nord:
-																										if( j-1 >=0){
-																												if(cmat->matrice[ i ][ j ].mur.murN !=1 )
-								                                            verif=1;
-																										}
-				                                				break;
-								                        		}
+								}while(verif == 0);
+							}
+						}
+						if(direction != Pas_besoin_de_bouger){
+							deplacer_canard(cmat,i, j, k, direction);
 
-                                    		}while(verif == 0);
-                                		}
-                            		}
-                            		if(direction != Pas_besoin_de_bouger){
-																		deplacer_canard(cmat,i, j, k, direction);
-																		fprintf(stderr,"\nFIN deplacer_canard\n");
-																}
-                            }
+						}
+		                    }
                         }
-                    }fprintf(stderr,"\nFIN POUR UN COINCOIN\n");
-        		}//fin balayage matrice
-				fprintf(stderr,"\nDEB deplacer_repro\n");
-        		reproduction(cmat, nourriture_accouplement, joueur,joueur2);
-        		fprintf(stderr,"\nFIN deplacer_repro\n");
-    				manger(cmat, nourriture_genere);
-						fprintf(stderr,"\nFIN deplacer_nour\n");
-    		}
+                    }
+        	}//fin balayage matrice
+				
+        	reproduction(cmat, nourriture_accouplement, joueur,joueur2);		
+    		manger(cmat, nourriture_genere);
+						
+    	}
 }
 
