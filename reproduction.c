b384012732ed034607a386a0492f6b324bd7fca5
Killian <l2info025@ic2s122-01.univ-lemans.fr>
2018-03-23 10:12:40 +0100

    changement de fonction ou pondre

--- reproduction.c
+++ reproduction.c
@@ -16,60 +16,63 @@ int ou_pondre(caract_mat_t * cmat, int * i, int * j){
 	int cpt =1;
 	while(1){ 
 		if(cmat->matrice[*i][*j].nb_occupant>8){ // Vu qu on ajoute un canard et qu on a 9 places il faut qu il y ait maximum 8 occupants
+			fprintf(stderr,"\n x %i y %i\n",*i,*j);
+			
 			if(est_dans_matrice(cmat,*i-cpt,*j)){
+				fprintf(stderr,"\n1\n");
 				if( cmat->matrice[*i-cpt][*j].nb_occupant<9){
 					*i=*i-cpt;
 					return 1;
+				}else if(est_dans_matrice(cmat,*i-cpt,*j-cpt)){
+					fprintf(stderr,"\n2\n");
+					if(cmat->matrice[*i-cpt][*j-cpt].nb_occupant<9){
+						*i=*i-cpt;
+						*j=*j-cpt;
+						return 1;
+					}else if(est_dans_matrice(cmat,*i,*j-cpt)){
+						fprintf(stderr,"\n3\n");
+						if( cmat->matrice[*i][*j-cpt].nb_occupant<9){
+							*j=*j-cpt;
+							return 1;
+						}else if(est_dans_matrice(cmat,*i+cpt,*j-cpt) ){
+							fprintf(stderr,"\n4\n");
+							if( cmat->matrice[*i+cpt][*j-cpt].nb_occupant<9){
+								*i=*i+cpt;
+								*j=*j-cpt;
+								return 1;
+							}else if(est_dans_matrice(cmat,*i+cpt,*j) ){
+								fprintf(stderr,"\n5\n");
+								if( cmat->matrice[*i+cpt][*j].nb_occupant<9){
+									*i=*i+cpt;
+									return 1;
+								}else if(est_dans_matrice(cmat,*i+cpt,*j+cpt) ){
+									fprintf(stderr,"\n6\n");
+									if( cmat->matrice[*i+cpt][*j+cpt].nb_occupant<9){
+										*i=*i+cpt;
+										*j=*j+cpt;
+										return 1;
+									}else if(est_dans_matrice(cmat,*i,*j+cpt) ){
+										fprintf(stderr,"\n7\n");
+										if( cmat->matrice[*i][*j+cpt].nb_occupant<9){
+											*j=*j+cpt;
+											return 1;
+										}else if(est_dans_matrice(cmat,*i-cpt,*j+cpt) ){
+											fprintf(stderr,"\n8\n");
+											if( cmat->matrice[*i-cpt][*j+cpt].nb_occupant<9){
+												*i=*i-cpt;
+												*j=*j+cpt;
+												return 1;
+											}
+										}
+									}
+								}
+							}
+						}
+					}
 				}
-fprintf(stderr,"\n1\n");
-			}else if(est_dans_matrice(cmat,*i-cpt,*j-cpt)){
-				if(cmat->matrice[*i-cpt][*j-cpt].nb_occupant<9){
-					*i=*i-cpt;
-					*j=*j-cpt;
-					return 1;
-				}
-fprintf(stderr,"\n2\n");
-			}else if(est_dans_matrice(cmat,*i,*j-cpt)){
-				if( cmat->matrice[*i][*j-cpt].nb_occupant<9){
-					*j=*j-cpt;
-					return 1;
-				}
-fprintf(stderr,"\n3\n");
-			}else if(est_dans_matrice(cmat,*i+cpt,*j-cpt) ){
-				if( cmat->matrice[*i+cpt][*j-cpt].nb_occupant<9){
-					*i=*i+cpt;
-					*j=*j-cpt;
-					return 1;
-				}
-fprintf(stderr,"\n4\n");
-			}else if(est_dans_matrice(cmat,*i+cpt,*j) ){
-				if( cmat->matrice[*i+cpt][*j].nb_occupant<9){
-					*i=*i+cpt;
-					return 1;
-				}
-fprintf(stderr,"\n5\n");
-			}else if(est_dans_matrice(cmat,*i+cpt,*j+cpt) ){
-				if( cmat->matrice[*i+cpt][*j+cpt].nb_occupant<9){
-					*i=*i+cpt;
-					*j=*j+cpt;
-					return 1;
-				}
-fprintf(stderr,"\n6\n");
-			}else if(est_dans_matrice(cmat,*i,*j+cpt) ){
-				if( cmat->matrice[*i][*j+cpt].nb_occupant<9){
-					*j=*j+cpt;
-					return 1;
-				}
-fprintf(stderr,"\n7\n");
-			}else if(est_dans_matrice(cmat,*i-cpt,*j+cpt) ){
-				if( cmat->matrice[*i-cpt][*j+cpt].nb_occupant<9){
-					*i=*i-cpt;
-					*j=*j+cpt;
-					return 1;
-				}fprintf(stderr,"\n8\n");
 			}
-		
-		}else{
+		}
+		else{
 			return 1;
 		}
 		cpt++;
