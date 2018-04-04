039d0db56298fe40a4e9138bdfbfc31d90916b66
Killian <l2info025@ic2s114-18.univ-lemans.fr>
2018-04-04 13:14:37 +0200

    fonction sdl

--- src/Fonction_SDL.h
+++ src/Fonction_SDL.h
@@ -1,10 +1,1 @@
-
-int choix_difficulte(SDL_Renderer *);
-
-int choix_nb_joueur(SDL_Renderer *);
-
-
-int option(SDL_Renderer *);
-
-
-int credit(SDL_Texture* ,SDL_Texture* ,SDL_Renderer*, SDL_Rect, SDL_Rect);
+void credit(SDL_Texture* ,SDL_Texture* ,SDL_Renderer*, SDL_Rect, SDL_Rect);
